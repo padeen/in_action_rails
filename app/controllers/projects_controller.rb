024638f44480e3022796d57a87fba58a761746da
Padeen <paulo.deen@gmail.com>
2020-01-28 01:14:28 +0100

    Create a new project feature complete

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,32 @@
+class ProjectsController < ApplicationController
+
+  def index
+    
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to @project, notice: "Project has been created."
+
+    else
+      # to be filled later
+
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end
+end
