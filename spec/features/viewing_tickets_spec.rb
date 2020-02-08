1f5ffe118dfd9d61532332349bbd989a20023cf6
Padeen <paulo.deen@gmail.com>
2020-02-08 13:36:49 +0100

    Add author to tickets in specs

--- spec/features/viewing_tickets_spec.rb
+++ spec/features/viewing_tickets_spec.rb
@@ -2,14 +2,16 @@ require "rails_helper"
 
 RSpec.feature "Users can view tickets" do
   before do
+    author = FactoryBot.create(:user)
+
     sublime = FactoryBot.create(:project, name: "Sublime Text 3")
     FactoryBot.create(:ticket, project: sublime,
-                      name: "Make it shiny!",
+                      author: author, name: "Make it shiny!",
                       description: "Gradients!, Starbursts! Oh my!")
 
     ie = FactoryBot.create(:project, name: "Internet Explorer")
     FactoryBot.create(:ticket, project: ie,
-                      name: "Standards compliance", description: "Isn't a joke.")
+                      author: author, name: "Standards compliance", description: "Isn't a joke.")
 
     visit "/"
   end
