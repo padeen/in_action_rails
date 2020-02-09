07c25f89769876069348014ae623c77ddf511c4b
Padeen <paulo.deen@gmail.com>
2020-02-09 17:27:48 +0100

    Hide delete project link to non-admin users

--- spec/features/hidden_links_spec.rb
+++ spec/features/hidden_links_spec.rb
@@ -1,13 +1,19 @@
 require "rails_helper"
 
 RSpec.feature "Users can only see the appropiate links" do
+  let(:project) { FactoryBot.create(:project) }
   let(:user) { FactoryBot.create(:user) }
   let(:admin) { FactoryBot.create(:user, :admin) }
 
   context "anonymous users" do
     scenario "cannot see the New Project link" do
-      visit "/"
+    visit "/"
       expect(page).to_not have_link "New Project"
+    end
+
+    scenario "cannot see the Delete Project link" do
+      visit project_path(project)
+      expect(page).to_not have_link "Delete Project"
     end
   end
 
@@ -15,6 +21,12 @@ RSpec.feature "Users can only see the appropiate links" do
     before { login_as(user) }
 
     scenario "cannot see the New Project link" do
+      visit "/"
+      expect(page).to_not have_link "New Project"
+    end
+
+    scenario "cannot see the Delete Project link" do
+      visit project_path(project)
       expect(page).to_not have_link "New Project"
     end
   end
@@ -25,6 +37,11 @@ RSpec.feature "Users can only see the appropiate links" do
     scenario "can see the New Project link" do
       visit "/"
       expect(page).to have_link "New Project"
+    end
+
+    scenario "can see the Delete Project link" do
+      visit project_path(project)
+      expect(page).to have_link "Delete Project"
     end
   end
 end
