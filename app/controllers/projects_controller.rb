ee54814a007c4fe3a5d8498e4064a5cdcfcc1760
Padeen <paulo.deen@gmail.com>
2020-02-09 16:41:56 +0100

    Add admin restriction to projects creation and deletion

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,28 +1,11 @@
 class ProjectsController < ApplicationController
-  before_action :set_project, only: [:show, :edit, :update, :destroy]
+  before_action :set_project, only: [:show, :edit, :update]
 
   def index
     @projects = Project.all
-    
   end
 
   def show
-  end
-
-  def new
-    @project = Project.new
-  end
-
-  def create
-    @project = Project.new(project_params)
-
-    if @project.save
-      redirect_to @project, notice: "Project has been created."
-
-    else
-      flash.now[:alert] = "Project has not been created."
-      render "new"
-    end
   end
 
   def edit
@@ -36,13 +19,6 @@ class ProjectsController < ApplicationController
       flash.now[:alert] = "Project has not been updated."
       render "edit"
     end
-  end
-
-  def destroy
-    @project.destroy
-
-    flash[:notice] = "Project has been deleted."
-    redirect_to projects_path
   end
 
   private
