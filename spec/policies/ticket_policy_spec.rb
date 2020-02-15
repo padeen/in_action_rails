8bdc7b29723b0c01bc2fc2e845d8edd0bc5c7818
Padeen <paulo.deen@gmail.com>
2020-02-15 14:28:19 +0100

    Restrict access to ticket to project members

--- spec/policies/ticket_policy_spec.rb
+++ spec/policies/ticket_policy_spec.rb
@@ -1,27 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe TicketPolicy, type: :policy do
-  let(:user) { User.new }
+  context "permissions" do
+    subject { TicketPolicy.new(user, ticket) }
 
-  subject { described_class }
+    let(:user) { FactoryBot.create(:user) }
+    let(:project) { FactoryBot.create(:project) }
+    let(:ticket) { FactoryBot.create(:ticket, project: project) }
 
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+    context "for anonymous users" do
+      let(:user) { nil }
 
-  permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+      it { should_not permit_action :show }
+    end
 
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+    context "for viewers of the project" do
+      before { assign_role!(user, :viewer, project) }
 
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+      it { should permit_action :show }
+    end
 
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
+    context "for editors of the project" do
+      before { assign_role!(user, :editor, project) }
+
+      it { should permit_action :show }
+    end
+
+    context "for managers of the project" do
+      before { assign_role!(user, :manager, project) }
+
+      it { should permit_action :show }
+    end
+
+    context "for managers of other projects" do
+      before do
+        assign_role!(user, :manager, FactoryBot.create(:project))
+      end
+
+      it { should_not permit_action :show }
+    end
+
+    context "for administrators" do
+      let(:user) { FactoryBot.create :user, :admin }
+
+      it { should permit_action :show }
+    end
   end
 end
