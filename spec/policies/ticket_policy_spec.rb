e1d2d151e24d4b073f5b056102c97b7a04206125
Padeen <paulo.deen@gmail.com>
2020-02-15 15:33:12 +0100

    Restrict ticket creation to admin, managers, editors

--- spec/policies/ticket_policy_spec.rb
+++ spec/policies/ticket_policy_spec.rb
@@ -6,30 +6,34 @@ RSpec.describe TicketPolicy, type: :policy do
 
     let(:user) { FactoryBot.create(:user) }
     let(:project) { FactoryBot.create(:project) }
-    let(:ticket) { FactoryBot.create(:ticket, project: project) }
+    let(:ticket) { FactoryBot.create(:ticket, author: user, project: project) }
 
     context "for anonymous users" do
       let(:user) { nil }
 
       it { should_not permit_action :show }
+      it { should_not permit_action :create }
     end
 
     context "for viewers of the project" do
       before { assign_role!(user, :viewer, project) }
 
       it { should permit_action :show }
+      it { should_not permit_action :create }
     end
 
     context "for editors of the project" do
       before { assign_role!(user, :editor, project) }
 
       it { should permit_action :show }
+      it { should permit_action :create }
     end
 
     context "for managers of the project" do
       before { assign_role!(user, :manager, project) }
 
       it { should permit_action :show }
+      it { should permit_action :create }
     end
 
     context "for managers of other projects" do
@@ -38,12 +42,14 @@ RSpec.describe TicketPolicy, type: :policy do
       end
 
       it { should_not permit_action :show }
+      it { should_not permit_action :create }
     end
 
     context "for administrators" do
       let(:user) { FactoryBot.create :user, :admin }
 
       it { should permit_action :show }
+      it { should permit_action :create }
     end
   end
 end
